9f27734483d1a393b8e9b90439facdb87417b3a3
Shaharyar <shery_kirmani@hotmail.com>
2019-09-05 23:59:53 +0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform, MenuController } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"]
 })
 export class AppComponent {
   public appPages = [
     {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
+      title: "Home",
+      url: "/home",
+      icon: "home"
     },
     {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
+      title: "List",
+      url: "/list",
+      icon: "list"
     }
   ];
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private menu: MenuController
   ) {
     this.initializeApp();
   }
@@ -36,5 +37,11 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  toggleMenu(id) {
+    console.log(id);
+    this.menu.enable(true, id);
+    this.menu.open(id);
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { MenuController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  // menu 1
+  loggedIn = false;
 
-  constructor() {}
+  constructor(private menu: MenuController) {}
 
+  openMenu(id) {
+    if (this.loggedIn) {
+      this.loggedIn = false;
+    } else {
+      this.loggedIn = true;
+    }
+
+    console.log(id);
+    this.menu.enable(true, id);
+    this.menu.open(id);
+  }
 }
